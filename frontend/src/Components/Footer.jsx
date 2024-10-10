1ef9e344017714c18287493a945ab438536dc3a3
Nisith Ferdinando <nisithsandaru@gmail.com>
2024-10-10 20:10:19 +0530

    initial commit

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='container'>
+            <div className='bg-Secondary w-full p-5 absolute bottom-0'>
+                <p className='text-white italic text-lg text-center'>@Copyright All Right Reserved</p>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import img from '../assets/img1.png'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='container'>
+            <div className='bg-Secondary flex justify-start gap-4 items-center'>
+                <img src={img} className='w-15 h-20 ml-8'/>
+                <p className='text-3xl text-white font-bold ml-24'>Sri Lanka Railway</p>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
