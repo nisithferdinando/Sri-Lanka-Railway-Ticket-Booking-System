09982e737e13e1eac554a293b22b9dca0745bd79
Nisith Ferdinando <nisithsandaru@gmail.com>
2025-01-12 19:03:33 +0530

    Review booking updated

---
+++ frontend/src/Utilities/LoadingOverlay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LoaderCircle  } from 'lucide-react';
+
+const LoadingOverlay = () => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/80 backdrop-blur-none">
+      <LoaderCircle className="h-24 w-24 text-blue-500 animate-spin" />
+    </div>
+  );
+};
+
+export default LoadingOverlay;
